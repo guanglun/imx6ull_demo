3a867b98110177c856b04d08a28e5f2447e760ff
gaunglun <272073020@qq.com>
2021-05-17 19:43:22 +0800

    init

---
+++ demo_udp/build.sh
@@ -0,0 +1,2 @@
+CC=/home/guanglun/imx6ull/ebf_6ull_buildroot/output/host/bin/arm-linux-gnueabihf-gcc 
+$CC main.c -o out/demo_udp
---
+++ demo_udp/exec.sh
@@ -0,0 +1,3 @@
+# !/bin/bash
+
+scp /out/demo_udp root@imx6ull.local:/root
